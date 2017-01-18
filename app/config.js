fdd732da5ce5a1e996e15e870d85a62d8f31213d
Hack Reactor Students <accounts@hackreactor.com>
2017-01-18 15:41:19 -0800

    Attempted refactor to Mongoose

--- app/config.js
+++ app/config.js
@@ -1,40 +1,78 @@
 var path = require('path');
-var knex = require('knex')({
-  client: 'sqlite3',
-  connection: {
-    filename: path.join(__dirname, '../db/shortly.sqlite')
-  },
-  useNullAsDefault: true
-});
-var db = require('bookshelf')(knex);
+// var knex = require('knex')({
+//   client: 'sqlite3',
+//   connection: {
+//     filename: path.join(__dirname, '../db/shortly.sqlite')
+//   },
+//   useNullAsDefault: true
+// });
+// var db = require('bookshelf')(knex);
 
-db.knex.schema.hasTable('urls').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('urls', function (link) {
-      link.increments('id').primary();
-      link.string('url', 255);
-      link.string('baseUrl', 255);
-      link.string('code', 100);
-      link.string('title', 255);
-      link.integer('visits');
-      link.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
+var mongoose = require('mongoose');
+var connection = mongoose.createConnection('mongodb://localhost/db_name');
+var Schema = mongoose.Schema;
+var autoIncrement = require('mongoose-auto-increment');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
+
+
+// Initialize auto-increment package
+autoIncrement.initialize(connection);
+
+var userSchema = new Schema({
+  name: String,
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  'createdAt': Date,
+  'updatedAt': Date
+});
+
+var urlSchema = new Schema({
+  url: { type: String, required: true },
+  baseUrl: { type: String, required: true },
+  code: { type: String, required: true },
+  title: { type: String, required: true },
+  visits: Number,
+  createdAt: Date,
+});
+
+userSchema.plugin(autoIncrement.plugin, 'User');
+urlSchema.plugin(autoIncrement.plugin, 'Link');
+
+userSchema.methods.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    callback(isMatch);
+  });
+};
+
+
+var User = mongoose.model('User', userSchema);
+var Link = mongoose.model('Link', urlSchema);
+
+// on every save, add the date
+userSchema.pre('save', function(next) {
+  var currentDate = new Date();
+  this.updatedAt = currentDate;
+  if (!this.createdAt) {
+    this.createdAt = currentDate;
   }
+
+  bcrypt.hash(this.password, null, null, function(err, hash) {
+    if (err) {
+      console.error(err);
+    } else {
+      this.password = hash;
+    }
+  }).bind(this);
+
+  next();
 });
 
-db.knex.schema.hasTable('users').then(function(exists) {
-  if (!exists) {
-    db.knex.schema.createTable('users', function (user) {
-      user.increments('id').primary();
-      user.string('username', 100).unique();
-      user.string('password', 100);
-      user.timestamps();
-    }).then(function (table) {
-      console.log('Created Table', table);
-    });
-  }
-});
+// urlSchema.pre('save', function(next) {
+//   // THIS IS WHERE WE CRYPTO THE CODE
+//   next();
+// });
 
-module.exports = db;
+module.exports.User = User;
+module.exports.Link = Link;
+
--- app/models/user.js
+++ app/models/user.js
@@ -23,3 +23,10 @@ var User = db.Model.extend({
 });
 
 module.exports = User;
+
+// the schema is useless so far
+// we need to create a model using it
+// var User = mongoose.model('User', userSchema);
+
+// make this available to our users in our Node applications
+// module.exports = User;
